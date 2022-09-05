2d0e9a632091fbb3a6b6fc839b860977bc0cd75b
Eulentier161 <63156923+Eulentier161@users.noreply.github.com>
2022-09-05 10:00:58 +0200

    cors
    resolves #5

--- pages/api/index.ts
+++ pages/api/index.ts
@@ -1,8 +1,22 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import prisma from '../../prisma/client';
 import { validateSlug, validateUrl } from '../../utils/validators';
+import Cors from 'cors';
+
+const cors = Cors({ methods: ['POST'] });
+
+function runMiddleware(req: NextApiRequest, res: NextApiResponse, fn: Function) {
+  return new Promise((resolve, reject) => {
+    fn(req, res, (result: any) => {
+      if (result instanceof Error) return reject(result);
+      return resolve(result);
+    });
+  });
+}
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  await runMiddleware(req, res, cors);
+
   const destination = req.body.destination;
   const slug = req.body.slug;
   if (!destination || !slug || typeof destination !== 'string' || typeof slug !== 'string' || req.method !== 'POST') {
@@ -25,5 +39,5 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     return res.status(500).json({ message: 'slug is already taken' });
   }
 
-  return res.status(200).json({ message: 'success!', url: `${req.headers.origin}/${slug}` });
+  return res.status(200).json({ message: 'success!', url: `${req.headers.host}/${slug}` });
 }
